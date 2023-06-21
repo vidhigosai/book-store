c2bdd2c1d54603f9ab6793188f1fb7b1da5de749
vidhigosai <108270421+vidhigosai@users.noreply.github.com>
2023-06-21 13:12:25 +0530

    Add files via upload

---
+++ routes/error.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const express = require('express');
+const router = express.Router();
+const errorController = require('../controllers/error');
+
+router.get('/',errorController.get404);
+
+module.exports = router;
